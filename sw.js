05fbed22cba0af1ddceeff15e43283a4133b7331
SuvarnaBhide <suvarna.p.bhide@gmail.com>
2026-05-28 01:02:40 +0530

    PWA-changes

--- sw.js
+++ sw.js
@@ -1,59 +1,53 @@
 const CACHE_NAME = "habit-tracker-v1";
 
 const urlsToCache = [
-    "/habit_tracker/",
-    "/habit_tracker/index.html",
-    "/habit_tracker/styles.css",
-    "/habit_tracker/app.js",
-    "/habit_tracker/manifest.json"
+  "/habit_tracker/",
+  "/habit_tracker/index.html",
+  "/habit_tracker/styles.css",
+  "/habit_tracker/app.js",
+  "/habit_tracker/manifest.json",
+  "/habit_tracker/icons/icon-1.png",
+  "/habit_tracker/icons/icon-2.png",
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
+  );
 
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
-
-    self.skipWaiting();
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        }),
+      );
+    }),
+  );
 
-    self.clients.claim();
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(cachedResponse => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-                return fetch(event.request)
-                    .then(networkResponse => {
-                        return caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(
-                                    event.request,
-                                    networkResponse.clone()
-                                );
-                                return networkResponse;
-                            });
-                    });
-            })
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    }),
+  );
 });
