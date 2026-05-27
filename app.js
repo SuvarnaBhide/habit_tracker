cd469c34c096bef05bfbda77e7eb473cc60c3ae8
SuvarnaBhide <suvarna.p.bhide@gmail.com>
2026-05-27 22:20:50 +0530

    .

--- app.js
+++ app.js
@@ -112,6 +112,6 @@ if ("serviceWorker" in navigator) {
 
   window.addEventListener("load", () => {
 
-    navigator.serviceWorker.register("./sw.js");
+    navigator.serviceWorker.register("/habit_tracker/sw.js");
   });
 }
